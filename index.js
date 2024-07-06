df2a54ef4e5b0355dbedc0045e932e4776cb8d9a
Ashmankevich <ashmankevich.a@gmail.com>
2024-07-06 18:20:29 +0300

    add tasks of objects

--- index.js
+++ index.js
@@ -563,3 +563,25 @@ function salariesEmployers(obj) {
 
 }
 salariesEmployers(salaries36)
+
+//Task 37
+
+let calculator = {
+	read() {
+		let a = +prompt('Please, enter the number a', 0)
+		let b = +prompt('Please, enter the number b', 0)
+
+		this.a = a
+		this.b = b
+	},
+	sum() {
+		return this.a + this.b
+	},
+	mul() {
+		return this.a * this.b
+	}
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
